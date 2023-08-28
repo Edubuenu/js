414f0e2707dce5ffb63d09c43e750929bc4d0689
edubuenu <edb199999@gmail.com>
2023-08-28 08:11:06 -0300

    //

--- Exercicio/exercicio2/script.js
+++ Exercicio/exercicio2/script.js
@@ -1,10 +1,17 @@
 var idade = "23"
 var sexo = "Masc"
 
-window.document.write(`${idade} ${sexo} `)
+document.write(`${idade} ${sexo} `)
 if ( idade <18 && seco == masc){
-    window.document.write(`É um jovem de ${idade} anos.`)
-} else if (idade > 18 < 60 && sexo == masc){
-    
+    document.write(`É um jovem de ${idade} anos.`)
+    img.src ="menino.jpg";
+    document.body.appendChild(img);
+} else if (idade > 18 < 60 && sexo == masc) {
+    document.write(`É um homem de ${idade} anos.`)
+    img.src ="adulto.jfif";
+    document.body.appendChild(img);
+} else if(idade > 60 && sexo == masc) {
+    window.document.write(`É um idoso de ${idade} anos.`)
 }
+
 window.document.write(`Preencha os dados para ver o resultado`)
